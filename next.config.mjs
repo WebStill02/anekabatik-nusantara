825e8863916f6a690c25726d2456e40f235d0b6c
Sara Sifra Gombo <saragombo42@gmail.com>
2025-07-14 22:40:04 +0700

    Apply final API fix and address key prop warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'i.pinimg.com', 
+        hostname: 'i.pinimg.com',
         port: '',
-        pathname: '/1200x/**',
+        pathname: '/**', // Gunakan '/**' untuk mencakup semua path di Pinterest
       },
       {
         protocol: 'https',
-        hostname: 'via.placeholder.com',
+        hostname: 'via.placeholder.com', // jika Anda menggunakan placeholder
         port: '',
         pathname: '/**',
       },
     ],
   },
 };
-
-export default nextConfig; 
+export default nextConfig; // <-- PASTIKAN INI export default
